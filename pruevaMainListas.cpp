2b55bd65ffe215128c2e54075bd5d2c87dd5708d
eliuthlarios <eliuthlario@gmail.com>
2023-10-04 20:01:22 -0600

    ":sparkles: nuevo mod:

---
+++ pruevaMainListas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <string.h>
+
+ struct datosDeJugador
+ {
+    string nombre;
+    string personaje;
+    int nivel;
+ };
+ 
+ struct Nodo
+ {
+    datosDeJugador jugadorx;
+    Nodo *siguiete;
+ };
+int main (){
+
+
+
+
+   return 0;
+}
+ 
