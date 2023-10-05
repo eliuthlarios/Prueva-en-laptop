9c8683b38be6fcdd1cdab0d7bbb5d9985b7a3cd5
eliuthlarios <eliuthlario@gmail.com>
2023-10-04 20:27:35 -0600

    :sparkles: nuevo code

--- pruevaMainListas.cpp
+++ pruevaMainListas.cpp
@@ -14,11 +14,33 @@ using namespace std;
     datosDeJugador jugadorx;
     Nodo *siguiete;
  };
+
+ void insertarlista(Nodo*&, datosDeJugador);
 int main (){
 
 
 
 
    return 0;
+}
+
+void insertarlista(Nodo *& lista, datosDeJugador jugadorY){
+   Nodo *nuevoNodo = new Nodo();
+   nuevoNodo->jugadorx = jugadorY;
+
+   Nodo *aux1= lista;
+   Nodo *aux2;
+
+   while ((aux1 != nullptr)&&(aux1->jugadorx < jugadorY)){
+      aux2 =aux1;
+      aux2 =aux1->siguiete;
+   }
+if(lista==aux1){
+   lista = nuevoNodo;
+   nuevoNodo->siguiete= aux1;
+}else{
+   aux2->siguiete = nuevoNodo;
+}
+nuevoNodo->siguiete = aux1;
 }
  
